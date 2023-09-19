ab1ca9008164b9270fb662f16726ee7f98d1ea8e
Parkchanhwi <56109340+ParkChanHwi@users.noreply.github.com>
2023-09-19 16:26:43 +0900

    SolvedQueuepro.cpp
    BAEKJOON 18258

--- Just_recording/Queuepro.cpp
+++ Just_recording/Queuepro.cpp
@@ -1,103 +1,104 @@
 #include <iostream>
 #include <string.h>
-#define MAX_LEN 2000000
+#define MAX_LEN 2000001
 
 typedef struct CQ_Queue {
-  int *Arr;
-  int Rear;
-  int Front;
+    int* Arr;
+    int Rear;
+    int Front;
 } CQ;
+int Empty(CQ* queue);
+void push(CQ* Queue, int data);
+int pop(CQ* Queue);
+int size(CQ* Queue);
+int Qfront(CQ* Queue);
+int Qback(CQ* Queue);
 
-CQ *createQue() {
-  CQ *cqueue = (CQ *)malloc(sizeof(CQ));
-  if (cqueue != NULL) {
-    cqueue->Arr = (int *)malloc(sizeof(int) * MAX_LEN);
-    if (cqueue->Arr == NULL)
-      free(cqueue);
-    return NULL;
-  } else {
-    cqueue->Front = cqueue->Rear = 0;
-  }
-  return cqueue;
-}
-int Empty(CQ *queue);
-
-void push(CQ *Queue, int data);
-
-int pop(CQ *Queue);
-
-int size(CQ *Queue);
-
-int Qfront(CQ *Queue);
-
-int Qback(CQ *Queue);
 
 int main() {
-  CQ *cQueue = createQue();
-  int c_num = 0;
-  char command[6];
-  scanf("%d", &c_num);
+    CQ cQueue;
+    cQueue.Front = 0;
+    cQueue.Rear = 0;
+    cQueue.Arr = (int*)malloc(sizeof(int) * MAX_LEN);
+    int c_num = 0;
+    char command[6];
+    scanf("%d", &c_num);
 
-  for (int i = 0; i < c_num; i++) {
-    scanf("%s", command);
-    if (strcmp("push", command) == 0) {
-      int x = 0;
-      scanf("%d", &x);
-      push(cQueue, x);
-    } else if (strcmp("pop", command) == 0)
-      printf("%d\n", pop(cQueue));
-  }
-}
-int Empty(CQ *queue) {
-  if (queue->Front == queue->Rear) {
-    return 1;
-  } else
-    return 0;
-}
+    for (int i = 0; i < c_num; i++) {
+        scanf("%s", command);
+        if (strcmp("push", command) == 0) {
+            int x = 0;
+            scanf("%d", &x);
+            push(&cQueue, x);
+        }
+        else if (strcmp("pop", command) == 0)
+            printf("%d\n", pop(&cQueue));
 
-void push(CQ *Queue, int data) {
-  int countRear = (Queue->Rear + 1) % MAX_LEN;
-  if (Queue != NULL && countRear != Queue->Front) // queue Isfull?
-  {
-    Queue->Arr[Queue->Rear] = data;
-    Queue->Rear = countRear;
-  }
+        else if (strcmp("size", command)==0)
+            printf("%d\n", size(&cQueue)-1);
+        else if (strcmp("empty", command) == 0)
+            printf("%d\n", Empty(&cQueue));
 
-  else
-    printf("Queue is Full");
+        else if (strcmp("front", command) == 0)
+            printf("%d\n", Qfront(&cQueue));
+
+        else if (strcmp("back", command) == 0)
+            printf("%d\n", Qback(&cQueue));
+    }
 }
 
-int pop(CQ *Queue) {
-  int Position = Queue->Front;
-
-  if (Empty(Queue) == 1)
-    return -1;
-
-  else {
-    Queue->Front++;
-  }
-  return Queue->Arr[Position];
+//여기부터 함수
+int Empty(CQ* queue) {
+    if (queue->Front == queue->Rear) {
+        return 1;
+    }
+    else
+        return 0;
 }
 
-int size(CQ *Queue) {
+void push(CQ* Queue, int data) {
+    int countRear = (Queue->Rear + 1) % MAX_LEN;
+    if (Queue != NULL && countRear != Queue->Front) // queue Isfull?
+    {
+        Queue->Arr[Queue->Rear] = data;
+        Queue->Rear = countRear;
+    }
 
-  return ((Queue->Rear - Queue->Front + 1) + MAX_LEN) % MAX_LEN;
+    else
+        printf("Queue is Full");
 }
 
-int Qfront(CQ *Queue) {
-  if (Empty(Queue) == 1)
-    return -1;
-  else {
+int pop(CQ* Queue) {
     int Position = Queue->Front;
+
+    if (Empty(Queue) == 1)
+        return -1;
+
+    else {
+        Queue->Front++;
+    }
     return Queue->Arr[Position];
-  }
 }
 
-int Qback(CQ *Queue) {
-  if (Empty(Queue) == 1)
-    return -1;
-  else {
-    int Position = Queue->Rear - 1;
-    return Queue->Arr[Position];
-  }
+int size(CQ* Queue) {
+
+    return ((Queue->Rear - Queue->Front + 1) + MAX_LEN) % MAX_LEN;
+}
+
+int Qfront(CQ* Queue) {
+    if (Empty(Queue) == 1)
+        return -1;
+    else {
+        int Position = Queue->Front;
+        return Queue->Arr[Position];
+    }
+}
+
+int Qback(CQ* Queue) {
+    if (Empty(Queue) == 1)
+        return -1;
+    else {
+        int Position = Queue->Rear - 1;
+        return Queue->Arr[Position];
+    }
 }
